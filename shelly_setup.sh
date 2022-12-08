1429cf2af161bc6981f67e4f51a7b9b7835abf75
Simon Büscher <sbuescher@brockhaus-ag.de>
2022-12-08 14:18:50 +0100

    Always restart containers

--- shelly_setup.sh
+++ shelly_setup.sh
@@ -1,4 +1,6 @@
-curl http://192.168.0.80/settings?mqtt_id=shellyht-wohnzimmer&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
-curl http://192.168.0.81/settings?mqtt_id=shellyht-schlafzimmer&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
-curl http://192.168.0.82/settings?mqtt_id=shellyht-buero&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
-curl http://192.168.0.83/settings?mqtt_id=shellyht-badr&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
+#!/bin/bash
+
+curl "http://192.168.0.80/settings?mqtt_id=shellyht-wohnzimmer&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true"
+curl "http://192.168.0.81/settings?mqtt_id=shellyht-schlafzimmer&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true"
+curl "http://192.168.0.82/settings?mqtt_id=shellyht-buero&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true"
+curl "http://192.168.0.83/settings?mqtt_id=shellyht-bad&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true"
