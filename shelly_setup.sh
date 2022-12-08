928156d18419c9fa5dab52ad4b351e24b5c36791
Simon Büscher <sbuescher@brockhaus-ag.de>
2022-12-08 12:45:14 +0100

    IPs and Shelly setup script

---
+++ shelly_setup.sh
@@ -0,0 +1,4 @@
+curl http://192.168.0.80/settings?mqtt_id=shellyht-wohnzimmer&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
+curl http://192.168.0.81/settings?mqtt_id=shellyht-schlafzimmer&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
+curl http://192.168.0.82/settings?mqtt_id=shellyht-buero&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
+curl http://192.168.0.83/settings?mqtt_id=shellyht-badr&mqtt_enable=true&mqtt_server=192.168.0.67:1883&mqtt_retain=true
